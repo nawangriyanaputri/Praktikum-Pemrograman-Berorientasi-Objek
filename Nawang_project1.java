58063ebaec3f42af229328f94639211386773985
nawangriyanaputri <nawangriyana578@gmail.com>
2025-09-01 11:39:02 +0700

    Add files via upload

---
+++ Nawang_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nawang_project1;
+
+/**
+ *
+ * @author MSii
+ */
+public class Nawang_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Nawang Riyana Putri");
+        System.out.println("NPM     : 2410506002");
+        System.out.println("Alamat  : Potrobangsan, Kec. Magelang Utara, Kota Magelang");
+        System.out.println("No_HP   : 023284923847");
+    }
+}
